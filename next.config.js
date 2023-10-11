3a66c2bab33327ddf84eaaae66c9de9fe2c870f1
Adel Ansari <adelansari.a@gmailcom>
2023-10-11 11:36:23 +0300

    setting up dependencies and configs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    redirects: async () => [
+      {
+        source: "/",
+        destination: "/channels/me",
+        permanent: true,
+      },
+    ],
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "**",
+        },
+      ],
+    },
+  };
+  
+  module.exports = nextConfig;
+  
